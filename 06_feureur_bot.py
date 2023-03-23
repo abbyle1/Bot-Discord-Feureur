0411edd4bdbf0f4cf8a90cd1bee1ba7816c7065c
@karzelguizio <kawrantin.arzel--guiziou@etudiant.univ-rennes1.fr>
2023-03-23 11:32:29 +0100

    mini correction 2

--- 06_feureur_bot.py
+++ 06_feureur_bot.py
@@ -104,6 +104,6 @@ async def on_message_delete(message):
     channel = message.channel
     deleted_message = message.content
     author = message.author.mention
-    await channel.send(f"{author}'s message has been deleted: {deleted_message.content}")
+    await channel.send(f"{author}'s message has been deleted: " + deleted_message.content)
 
 client.run("MTA1MzIyOTI5ODIxMjk0MTg1NA.GtTYrZ.6WLOt582X4EKzGD5sudvlp-uGaLgGBNS0fh5SU")
